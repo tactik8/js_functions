8a9cca86b1b5f23fa3f949aec5f78a6fe0c21734
tactik8 <48621231+tactik8@users.noreply.github.com>
2026-06-12 12:04:38 -0400

    Adding  appendQueryParams

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ export * from './extractEmails/extractEmails.js'
 
 import * as parseUrlParameters from './parseUrlParameters/parseUrlParameters.js'
 export * from './parseUrlParameters/parseUrlParameters.js'
+
+import * as appendQueryParams from './appendQueryParams/appendQueryParams.js'
+export * from './appendQueryParams/appendQueryParams.js'
