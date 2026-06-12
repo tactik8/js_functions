77e19e578c01f11a1c5d2317d9d269891ad8558b
tactik8 <48621231+tactik8@users.noreply.github.com>
2026-06-12 12:48:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-import * as addNumbers from './addNumbers/addNumbers.js'
-export * from './addNumbers/addNumbers.js'
 import * as extractEmails from './extractEmails/extractEmails.js'
 export * from './extractEmails/extractEmails.js'
 
