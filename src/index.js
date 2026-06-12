efdab899f55d7f4d443b1c7fe831999f96fd7317
tactik8 <48621231+tactik8@users.noreply.github.com>
2026-06-12 12:03:13 -0400

    Adding  parseUrlParameters

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import * as addNumbers from './addNumbers/addNumbers.js'
 export * from './addNumbers/addNumbers.js'
 import * as extractEmails from './extractEmails/extractEmails.js'
 export * from './extractEmails/extractEmails.js'
+
+import * as parseUrlParameters from './parseUrlParameters/parseUrlParameters.js'
+export * from './parseUrlParameters/parseUrlParameters.js'
