d06ad185180d89aea90235952a55016a6bb4554f
tactik8 <48621231+tactik8@users.noreply.github.com>
2026-06-12 12:00:25 -0400

    Adding  extractEmails

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 
 import * as addNumbers from './addNumbers/addNumbers.js'
 export * from './addNumbers/addNumbers.js'
+import * as extractEmails from './extractEmails/extractEmails.js'
+export * from './extractEmails/extractEmails.js'
